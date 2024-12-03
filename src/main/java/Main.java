7e1bfcb4457e4e6b3f6f4e0ec53606fdf270cfa6
Dhananjay Shettigar <dhananjay.s@juspay.in>
2024-12-03 21:16:21 +0530

    added multiple request handling with default response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,69 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 
 public class Main {
+  private final static int PORT = 4221;
+  private final static int THREAD_POOL_SIZE = 10;
+
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
+    try (ServerSocket serverSocket = new ServerSocket(PORT); ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE)) {
+      // Since the tester restarts your program quite often, setting SO_REUSEADDR
+      // ensures that we don't run into 'Address already in use' errors
+      serverSocket.setReuseAddress(true);
 
-     try (ServerSocket serverSocket = new ServerSocket(4221)) {
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
+      while (true) {
+        Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+        System.out.println("Accepted incoming socket connection: " + clientSocket.getRemoteSocketAddress());
 
-       Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
-       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+        executor.submit(() -> handleIncomingConnection(clientSocket));
+      }
 
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    }
+  }
+
+  public static void handleIncomingConnection(Socket clientSocket) {
+    try (BufferedReader socketReadBuffer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); PrintWriter socketWriter = new PrintWriter(clientSocket.getOutputStream(), true)) {
+      String requestLine = socketReadBuffer.readLine();
+
+      if (requestLine == null) {
+        System.out.println("Received preflight / handshake request. Closing socket " + clientSocket.getRemoteSocketAddress());
+        clientSocket.close();
+        return;
+      }
+      String[] requestHTTPTokens = requestLine.split(" ");
+      System.out.println("Request received: " + Arrays.toString(requestHTTPTokens));
+
+      if (requestHTTPTokens.length == 3) {
+        switch (requestHTTPTokens[1]) {
+          case "/":
+            socketWriter.println("HTTP/1.1 200 OK\r\n\r\n");
+            break;
+          default:
+            socketWriter.println("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+        clientSocket.close();
+      } else {
+        throw new IOException("Invalid HTTP Request Format");
+      }
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    }
   }
 }
