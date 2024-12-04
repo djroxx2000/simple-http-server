26bee901913134640382f82fb83661c2769a45ed
Dhananjay Shettigar <dhananjay.s@juspay.in>
2024-12-04 12:04:20 +0530

    added echo path handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,9 @@ import java.util.stream.Stream;
 public class Main {
   private final static int PORT = 4221;
   private final static int THREAD_POOL_SIZE = 10;
+  private final static String RESPONSE_VERSION = "HTTP/1.1 ";
+  private final static String SUCCESS = "200 OK";
+  private final static String NOT_FOUND = "404 Not Found";
 
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -51,12 +54,14 @@ public class Main {
       System.out.println("Request received: " + Arrays.toString(requestHTTPTokens));
 
       if (requestHTTPTokens.length == 3) {
-        switch (requestHTTPTokens[1]) {
-          case "/":
-            socketWriter.println("HTTP/1.1 200 OK\r\n\r\n");
-            break;
-          default:
-            socketWriter.println("HTTP/1.1 404 Not Found\r\n\r\n");
+
+        final String path = requestHTTPTokens[1];
+        if (path.equals("/")) {
+          socketWriter.println(RESPONSE_VERSION + SUCCESS + "\r\n\r\n");
+        } else if (path.startsWith("/echo")) {
+          socketWriter.println(handleEchoPath(path));
+        } else {
+          socketWriter.println(RESPONSE_VERSION + NOT_FOUND + "\r\n\r\n");
         }
         clientSocket.close();
       } else {
@@ -65,5 +70,19 @@ public class Main {
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
+  }
+
+  public static StringBuilder handleEchoPath(final String path) throws IOException {
+    final String[] echoResp = path.split("/echo/");
+    String[] headers = {"Content-Type: text/plain", "Content-Length: " + echoResp[1].length()};
+    if (echoResp.length == 2) {
+      StringBuilder respBuilder = new StringBuilder();
+      respBuilder.append(RESPONSE_VERSION).append(SUCCESS).append("\r\n");
+      for (final String header : headers) {
+        respBuilder.append(header).append("\r\n");
+      }
+      return respBuilder.append("\r\n").append(echoResp[1]);
+    }
+    throw new IOException("Echo path is malformed");
   }
 }
