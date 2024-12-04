b2c5dea55f802cf077a02d4fc1bbc9c861811949
Dhananjay Shettigar <dhananjay.s@juspay.in>
2024-12-04 15:26:16 +0530

    added user-agent path handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,7 @@ import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Stream;
@@ -16,9 +14,12 @@ import java.util.stream.Stream;
 public class Main {
   private final static int PORT = 4221;
   private final static int THREAD_POOL_SIZE = 10;
+
   private final static String RESPONSE_VERSION = "HTTP/1.1 ";
   private final static String SUCCESS = "200 OK";
   private final static String NOT_FOUND = "404 Not Found";
+
+  private final static String USER_AGENT = "user-agent";
 
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -44,22 +45,33 @@ public class Main {
   public static void handleIncomingConnection(Socket clientSocket) {
     try (BufferedReader socketReadBuffer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); PrintWriter socketWriter = new PrintWriter(clientSocket.getOutputStream(), true)) {
       String requestLine = socketReadBuffer.readLine();
-
       if (requestLine == null) {
         System.out.println("Received preflight / handshake request. Closing socket " + clientSocket.getRemoteSocketAddress());
         clientSocket.close();
         return;
       }
       String[] requestHTTPTokens = requestLine.split(" ");
-      System.out.println("Request received: " + Arrays.toString(requestHTTPTokens));
+      Map<String, String> headers = new HashMap<>();
+      String nextLine;
+      while((nextLine = socketReadBuffer.readLine()) != null) {
+        if (nextLine.isEmpty()) {
+          break;
+        }
+        final int headerSplitIndex = nextLine.indexOf(": ");
+        final String headerKey = nextLine.substring(0, headerSplitIndex).toLowerCase();
+        final String headerValue = nextLine.substring(headerSplitIndex + 2);
+        headers.put(headerKey, headerValue);
+      }
+      System.out.println("Request received: " + Arrays.toString(requestHTTPTokens) + "\n" + headers);
 
       if (requestHTTPTokens.length == 3) {
-
         final String path = requestHTTPTokens[1];
         if (path.equals("/")) {
           socketWriter.println(RESPONSE_VERSION + SUCCESS + "\r\n\r\n");
         } else if (path.startsWith("/echo")) {
           socketWriter.println(handleEchoPath(path));
+        } else if (path.equals("/user-agent")) {
+          socketWriter.println(returnHeader(headers, USER_AGENT));
         } else {
           socketWriter.println(RESPONSE_VERSION + NOT_FOUND + "\r\n\r\n");
         }
@@ -74,8 +86,8 @@ public class Main {
 
   public static StringBuilder handleEchoPath(final String path) throws IOException {
     final String[] echoResp = path.split("/echo/");
-    String[] headers = {"Content-Type: text/plain", "Content-Length: " + echoResp[1].length()};
     if (echoResp.length == 2) {
+      String[] headers = {"Content-Type: text/plain", "Content-Length: " + echoResp[1].length()};
       StringBuilder respBuilder = new StringBuilder();
       respBuilder.append(RESPONSE_VERSION).append(SUCCESS).append("\r\n");
       for (final String header : headers) {
@@ -84,5 +96,19 @@ public class Main {
       return respBuilder.append("\r\n").append(echoResp[1]);
     }
     throw new IOException("Echo path is malformed");
+  }
+
+  public static StringBuilder returnHeader(final Map<String, String> headerMap, final String respHeader) throws IOException {
+    if (headerMap.get(respHeader) != null) {
+      String[] headers = {"Content-Type: text/plain", "Content-Length: " + headerMap.get(respHeader).length()};
+      StringBuilder respBuilder = new StringBuilder();
+      respBuilder.append(RESPONSE_VERSION).append(SUCCESS).append("\r\n");
+      for (final String header : headers) {
+        respBuilder.append(header).append("\r\n");
+      }
+      respBuilder.append("\r\n").append(headerMap.get(respHeader));
+      return respBuilder;
+    }
+    throw new IOException("User Agent Path request received without user-agent header");
   }
 }
